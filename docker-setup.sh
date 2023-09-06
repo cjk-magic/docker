1a7559012072b21fc3035a0fde354b046a9b3ead
cjk-magic <139297954+cjk-magic@users.noreply.github.com>
2023-09-06 14:19:48 +0900

    Update docker-setup.sh

--- docker-setup.sh
+++ docker-setup.sh
@@ -2,7 +2,7 @@
 apt-get update
 apt-get install -y ca-certificates curl gnupg sudo
 sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o -y /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 echo \
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
